8b137a32e859db9cf39eb03897a3a923eef82e72
aleeMR <alee_1225@hotmail.com>
2022-12-08 17:35:09 -0500

    Add Challenge 6

---
+++ 06-Day/challenge6.js
@@ -0,0 +1,29 @@
+// Función para crear un cubo de 3x3x3
+function createCube(size) {
+  const cubeTop = Array.from(
+      {length: size}, 
+      (v, i) => (' ').repeat(size-i-1) + ('/\\').repeat(i+1) + ('_\\').repeat(size)
+    );
+  const cubeBottom = Array.from(
+      {length: size}, 
+      (v, i) => (' ').repeat(i) + ('\\/').repeat(size-i) + ('_/').repeat(size)
+    );
+
+  return [...cubeTop, ...cubeBottom].join('\n');
+}
+
+// Lista de pruebas
+// compuesta por el tamaño del cubo
+const tests = [
+    { size: 1 },
+    { size: 2 },
+    { size: 3 },
+    { size: 10 }
+  ];
+
+tests.forEach(test => {
+  // Llamada a la función
+  const cube = createCube(test.size);
+  // Resultado de la función
+  console.log(cube);
+});
